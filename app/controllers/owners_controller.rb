4dc67fe8c9fc26215b71f5ddcf4b2d8aba7ca7cd
chris <cseiter@gmail.com>
2021-10-08 19:56:12 -0400

    owners route successfully created

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,2 +1,30 @@
 class OwnersController < ApplicationController
+    def index
+        owners = Owner.all
+        render json: owners
+    end
+
+    def show
+        owner = Owner.find(params[:id])
+        render json: owner
+    end
+
+    def create
+        owner = Owner.create!(owner_params)
+        render json: owner, status: :created
+    end
+
+    def destroy
+        owner = Owner.find_by(id: params[:id])
+        if owner
+          owner.destroy
+          head :no_content
+        else
+          render json: { error: "Owner not found" }, status: :not_found
+        end
+    end       
+
+    def owners_params
+        params.permit(:owner_name)
+    end
 end
