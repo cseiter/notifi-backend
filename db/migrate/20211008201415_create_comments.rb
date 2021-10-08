bc2e426263f5c254323924a7a0258b412681bc06
chris <cseiter@gmail.com>
2021-10-08 17:55:23 -0400

    remove manual creation of foreign keys

--- db/migrate/20211008201415_create_comments.rb
+++ db/migrate/20211008201415_create_comments.rb
@@ -2,9 +2,8 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
       t.string :comment
-      t.references :users_id
+
       t.timestamps
     end
-    add_foreign_key :comments, :users, column: :users_id
   end
 end
--- db/migrate/20211008201454_create_tickets.rb
+++ db/migrate/20211008201454_create_tickets.rb
@@ -3,16 +3,7 @@ class CreateTickets < ActiveRecord::Migration[6.1]
     create_table :tickets do |t|
       t.string :ticket_title
       t.string :ticket_details
-      t.references :devices_id
-      t.references :owners_id
-      t.references :stations_id
-      t.references :comments_id
       t.timestamps
     end
-    add_foreign_key :tickets, :devices, column: :devices_id
-    add_foreign_key :tickets, :owners, column: :owners_id
-    add_foreign_key :tickets, :stations, column: :stations_id
-    add_foreign_key :tickets, :statuses, column: :statuses_id
-    add_foreign_key :tickets, :comments, column: :comments_id
   end
 end
