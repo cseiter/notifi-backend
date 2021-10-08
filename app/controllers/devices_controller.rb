15f0f9bae4ce96c543ce3e923173c2c59a9c9c1f
chris <cseiter@gmail.com>
2021-10-08 16:20:19 -0400

    add routes to devices_controller

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,2 +1,30 @@
 class DevicesController < ApplicationController
+    def index
+        devices = Device.all
+        render json: devices
+    end
+
+    def show
+        devices = Device.find(params[:id])
+        render json: devices
+    end
+
+    def create
+        device = Device.create!(device_params)
+        render json: device, status: :created
+    end
+
+    def destroy
+        device = Device.find_by(id: params[:id])
+        if device
+          device.destroy
+          head :no_content
+        else
+          render json: { error: "Device not found" }, status: :not_found
+        end
+    end       
+
+    def device_params
+        params.permit(:device_type)
+    end
 end
