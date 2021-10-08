92169aac6aafa81f549715f0a905cffc276ed0ff
chris <cseiter@gmail.com>
2021-10-08 17:40:16 -0400

    added child record destroy to models

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,7 +1,7 @@
 class Ticket < ApplicationRecord
-    has_one :device
-    has_one :owner
-    has_one :station
-    has_one :status
-    has_many :tickets
+    has_one :device, dependent: :destroy
+    has_one :owner, dependent: :destroy
+    has_one :station, dependent: :destroy
+    has_one :status, dependent: :destroy
+    has_many :tickets, dependent: :destroy
 end
