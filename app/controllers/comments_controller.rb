0d8deec385e9f0fe0223e07a19be117a34b2b4fa
chris <cseiter@gmail.com>
2021-10-08 20:47:27 -0400

    comments_controller updated and initial test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,30 @@
 class CommentsController < ApplicationController
+    def index
+        comments = Comment.all
+        render json: comments
+    end
+
+    def show
+        comment = Comment.find(params[:id])
+        render json: comment
+    end
+
+    def create
+        comment = Comment.create!(comment_params)
+        render json: comment, status: :created
+    end
+
+    def destroy
+        comment = Comment.find_by(id: params[:id])
+        if comment
+          comment.destroy
+          head :no_content
+        else
+          render json: { error: "Comment not found" }, status: :not_found
+        end
+    end       
+
+    def comment_params
+        params.permit(:comment, :users_id, :tickets_id, :created_at)
+    end
 end
