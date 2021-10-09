95fe704f964daaf87c23ff45bd8a6dec959bafa9
chris <cseiter@gmail.com>
2021-10-08 20:51:28 -0400

    statuses controller updated and initial test

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,2 +1,30 @@
 class StatusesController < ApplicationController
+    def index
+        statuses = Status.all
+        render json: statuses
+    end
+
+    def show
+        status = Status.find(params[:id])
+        render json: status
+    end
+
+    def create
+        status = Status.create!(status_params)
+        render json: status, status: :created
+    end
+
+    def destroy
+        status = Status.find_by(id: params[:id])
+        if status
+          status.destroy
+          head :no_content
+        else
+          render json: { error: "Status not found" }, status: :not_found
+        end
+    end       
+
+    def owners_params
+        params.permit(:status_level)
+    end
 end
