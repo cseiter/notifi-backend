931c021e92583f087b17c2f60f9369111d66b0db
chris <cseiter@gmail.com>
2021-10-08 20:33:18 -0400

    stations controller updated

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,2 +1,30 @@
 class StationsController < ApplicationController
+    def index
+        stations = Station.all
+        render json: stations
+    end
+
+    def show
+        station = Station.find(params[:id])
+        render json: station
+    end
+
+    def create
+        station = Station.create!(station_params)
+        render json: station, status: :created
+    end
+
+    def destroy
+        station = Station.find_by(id: params[:id])
+        if station
+          station.destroy
+          head :no_content
+        else
+          render json: { error: "Station not found" }, status: :not_found
+        end
+    end       
+
+    def station_params
+        params.permit(:station_type)
+    end
 end
