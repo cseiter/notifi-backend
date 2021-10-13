32d0e3a54cbbaf63d8f64174e52124eebe0b4d36
chris <cseiter@gmail.com>
2021-10-13 12:20:43 -0400

    edit cors to add update to methods

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://127.0.0.1:3001'
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head, :update]
   end
 end
